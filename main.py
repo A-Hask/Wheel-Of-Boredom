babb2c671232adafb7c6905a6ed12db24b243391
A-Hask <ahask93@gmail.com>
2023-10-10 16:45:42 -0400

    app complete

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 from tkinter import *
-from tkinter import messagebox
 import random
-from PIL import ImageTk,Image
+from PIL import ImageTk, Image
 
 root = Tk()
 root.title("WHEEL! OF! BOREDOM!")
-# root.iconbitmap('C:/Users/User/Documents/IvyTech/Fall2023/SDEV140-11P/Final-Project/Wheel-Of-Boredom/images')
-
-# icon=ImageTk.PhotoImage(Image.open("wheel.png"))
 
 # Widgets
 welcome = Label(root, text="Welcome to the")
@@ -15,35 +11,51 @@ title = Label(root, text="Wheel of Boredom!")
 viewOrSurprise = Label(root, text="Would you like to view the list before you spin?")
 
 # List
-activities = ["go for a walk", "watch your favorite show", "go see a movie", "draw something", "play a video game", 
-                  "take a long bath", "go for a drive", "play with a pet", "chat with a friend", "have a tea party", 
-                  "complete one task that you've been putting off", "listen to music", "try a new recipe", 
-                  "visit the library", "try a new restaurant"]
+activities = ["go for a walk", "watch your favorite show", "go see a movie", "draw something", "play a video game",
+              "take a bubble bath", "go for a drive", "play with a pet", "chat with a friend", "have a tea party",
+              "complete one task that you've been putting off", "listen to music", "try a new recipe",
+              "visit the library", "try a new restaurant", "take a nap", "write a poem", "visit a cafe",
+              "go to an ice cream shop", "start learning a new hobby", "organize your clothes", "build a fort"]
 
 # Functions
-# Display the list
-def viewList ():
-    Label(root, text=activities)
+# Popup window to display the list
+def viewList():
+    activityList = Toplevel()
+    activityList.title("Activity List!")
+    activityList.geometry("300x600")
+    words = Label(activityList, text="Here is the list of activities:")
+    words.pack()
+    for choice in activities:
+        actList = Label(activityList, text=str(choice))
+        actList.pack()
+    back = Button(activityList, text="Back to homepage", command=activityList.destroy, padx=5, pady=5)
+    back.pack(padx=5, pady=5)
+
 # Randomize the array and display the result in a pop up window
 def randomizer():
+    randomize = Toplevel()
+    randomize.title("Here is your Result!")
+    randomize.geometry("300x100")
     result = random.choice(activities)
-    messagebox.showinfo("Congratulations! You should " + result + "!")
 
+    announcement = Label(randomize, text="I think that you should... " + result + "!")
+    announcement.pack()
+
+    back = Button(randomize, text="Back to home page", command=randomize.destroy )
+    back.pack(padx=5, pady=5)
 
 # Buttons
-seeTheList =Button(root, text="See the list", command=viewList)
+seeTheList = Button(root, text="See the list", command=viewList)
 surpriseMe = Button(root, text="Surprise me", command=randomizer)
-spin = Button(root, text="Spin the Wheel!", command=randomizer)
 exit = Button(root, text="Exit", command=root.quit)
 
-
-# Widget Placement
+# Main Window Widget Placement
 welcome.grid(row=0, column=1)
 title.grid(row=1, column=1)
 viewOrSurprise.grid(row=2, column=1)
-seeTheList.grid(row=3, column=0)
-surpriseMe.grid(row=3, column=2)
-exit.grid(row=8, column=1)
+seeTheList.grid(row=3, column=0, padx=5, pady=5)
+surpriseMe.grid(row=3, column=2, padx=5, pady=5)
+exit.grid(row=8, column=1, padx=5, pady=5)
 
 
 root.mainloop()
